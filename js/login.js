dfba16975fc6b1815b1749633a3698211fdf9e18
Joel <valentinfernandez2010@hotmail.es>
2020-08-13 21:46:13 -0300

    Modificacion de index, login y products HTML. Tambien se modificaron login y products JS. Nuevos archivos: products y estiloslogin CSS.

--- js/login.js
+++ js/login.js
@@ -1,6 +1,18 @@
+//    PRUEBA DE VALIDACION DESDE JS
+//console.log(document.getElementById('correo').value);
+//console.log(document.getElementById('contraseña').value)
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById('formulario').addEventListener('submit', (evento)=> {
+        evento.preventDefault();
+        location.href = "./index.html";
+        sessionStorage.setItem("logeado", true);
+        return true;
+    })
 });
--- js/products.js
+++ js/products.js
@@ -1,3 +1,21 @@
+getJSONData(PRODUCTS_URL).then(function(datos){
+    console.log(datos);
+    for (let i=0; i < datos.data.length; i++){
+    document.getElementById('pro-list-container').innerHTML +=
+     `<div class="contenedor">`+
+        `<h2 class="nombre">`+ datos.data[i].name +`</h2>` + 
+        `<div class="cantVendidos">`+ `Vendidos: `+ datos.data[i].soldCount+`</div>`+
+     `</div>`+
+     `<div class="imagenDescrip">`+
+        `<img src="`+datos.data[i].imgSrc+`">`+
+        `<p class="descripcion">`+ datos.data[i].description+ `</p>`+ `<br><br>`+
+     `</div>`+
+     `<div class="precio">`+ `Precio: `+ datos.data[i].currency+ ` `+datos.data[i].cost+
+     `</div>`
+     ;
+    }
+})    
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
